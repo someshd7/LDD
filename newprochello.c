923b900e95296999a65cb2c98af78b383d697649
AsimMerchant <asimthatsme@gmail.com>
2020-04-28 18:57:08 +0530

    modified

--- newprochello.c
+++ newprochello.c
@@ -2,20 +2,32 @@
 
 struct proc_dir_entry *some_file; 
 
+int prochello_open (struct inode *_inode, struct file *_file)
+{
+	pr_alert("Opened\n");
+	return 0;
+}
+
+static struct file_operations fops = {
+
+.open = prochello_open,
+
+};
+
 static int prochello_init(void)
 {
-	some_file=proc_create("prochello", 0666, NULL,NULL);
-	if (some_file==NULL)
+	some_file=proc_create("newprochello", S_IRWXU | S_IRWXG | S_IRWXO, NULL,&fops);
+	if (some_file)
 	{
-		pr_alert("Failure\n");
-		return 42;
+		pr_alert("Success\n");
+		return 0;
 	}
-	return 0;
+	return -42;
 }
 
 static void prochello_exit(void)
 {
-	remove_proc_entry("prochello",some_file);
+	remove_proc_entry("prochello",NULL);
 }
 
 module_init(prochello_init);
---
+++ testproc.c
@@ -0,0 +1,26 @@
+#include "kernHeaders.h"
+
+static int hello_proc_open(struct inode *inode, struct  file *file) {
+  return 0;
+}
+
+static const struct file_operations hello_proc_fops = {
+  .owner = THIS_MODULE,
+  .open = hello_proc_open,
+  .read = seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,
+};
+
+static int __init hello_proc_init(void) {
+  proc_create("hello_proc", 0, NULL, &hello_proc_fops);
+  return 0;
+}
+
+static void __exit hello_proc_exit(void) {
+  remove_proc_entry("hello_proc", NULL);
+}
+
+MODULE_LICENSE("GPL");
+module_init(hello_proc_init);
+module_exit(hello_proc_exit);
